4d00f0e4bd99ac1da79c16bd0d2d9072785df24c
Frédéric Carisey <frederic.carisey@xorivia.com>
2022-01-10 21:17:30 +0100

    up

--- librairie/View/home.php
+++ librairie/View/home.php
@@ -1,3 +1,34 @@
+<?php
+
+$internships = \Controller\InternshipController::SELECT(\Database::SELECT_ALL, null, 4);
+$internships = [$internships[0], $internships[0], $internships[0], $internships[0]];
+$x = 0;
+
+?>
+
 <div id="home">
-    
+    <div class="internshipsProposal">
+        <div>
+            <h1>Proposition de stage</h1>
+            <div class="internships">
+            <?php foreach($internships as $internship): ?>
+                <?php if ($x == 0): ?>
+                    <div class="internshipRow">
+                <?php endif ?>
+
+                <?php $x++ ?>
+                <div class="internship">
+                    <h2><?= $internship->getDesignation() ?></h2>
+                    <p><?= $internship->getDescription() ?></p>
+                </div>
+
+                <?php if ($x == 2): $x = 0 ?>
+                    </div>
+                <?php endif ?>
+            <?php endforeach ?>
+            </div>
+            
+            <p class="seeMore"><a href="/internships">Voir plus</a></p>
+        </div>
+    </div>
 </div>
