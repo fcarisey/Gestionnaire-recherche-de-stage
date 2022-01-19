2ba7300aba316aff614602a43a699327aa411a45
Frédéric Carisey <63855888+Venntus17@users.noreply.github.com>
2022-01-19 09:48:19 +0100

    mini resp

--- librairie/View/home.php
+++ librairie/View/home.php
@@ -3,6 +3,8 @@
 $internships = \Controller\InternshipController::SELECT(\Database::SELECT_ALL, null, 4);
 $internships = [$internships[0], $internships[0], $internships[0], $internships[0]];
 
+$x = 0;
+
 ?>
 
 <div id="home">
@@ -10,11 +12,21 @@ $internships = [$internships[0], $internships[0], $internships[0], $internships[
         <h1>Proposition de stage</h1>
         <div class="internships">
         <?php foreach($internships as $internship): ?>
+            <?php if ($x == 0): ?>
+                <div>
+            <?php endif ?>
+
+            <?php $x++; ?>
+
             <div class="internship">
                 <h2><?= $internship->getDesignation() ?></h2>
                 <p><?= $internship->getDescription() ?></p>
                 <p class="seeMore"><a href="./internship/<?= $internship->getIdInternship() ?>">Voir plus</a></p>
             </div>
+
+            <?php if ($x == 2): $x = 0; ?>
+                </div>
+            <?php endif ?>
         <?php endforeach ?>
         </div>
             
