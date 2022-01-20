247c6a23483f5f9e3d0ac7415f0fc4de9ea6533b
Frédéric Carisey <63855888+Venntus17@users.noreply.github.com>
2022-01-20 09:04:13 +0100

    Update contact.php

--- librairie/View/contact.php
+++ librairie/View/contact.php
@@ -1,1 +1,20 @@
-<h2>Contact</h2>
+<div id="contact">
+    <form method="post" action="./">
+        <div>
+            <label>
+                Objet:
+                <input type="text" name="object">
+            </label>
+            <?php if (isset($_SESSION['id'])): ?>
+                <label>
+                    Courriel:
+                    <input type="email" name="courriel">
+                </label>
+            <?php endif />
+        </div>
+        <label>
+            Message:
+            <textbox name="message"></textbox>
+        </label>
+    </form>
+<div>
