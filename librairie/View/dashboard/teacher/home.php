29fd5756e2266010ef02b17c3faf13263dba6009
Frédéric Carisey <frederic.carisey@xorivia.com>
2022-02-19 12:05:12 +0100

    teacher dashboard home

--- librairie/View/dashboard/teacher/home.php
+++ librairie/View/dashboard/teacher/home.php
@@ -1,1 +1,22 @@
-<h2>dashboard/teacher</h2>
+<div id='dt_home'>
+    <div>
+        <div>
+            <h3>Elèves ayant trouvé un stage</h3>
+            <div>
+                <p>18%</p>
+                <img src="https://chart.apis.google.com/chart?chs=200x200&chdlp=b&chd=t:82,18&cht=p&chdl=Non%20trouv%C3%A9|Trouv%C3%A9&chdlp=&chdls=000000,12" alt="chart p">
+            </div>
+        </div>
+        <div>
+            <h3>Elèves interessé par un stage proposé</h3>
+            <div>
+                <p>86%</p>
+                <img src="https://chart.apis.google.com/chart?chs=200x200&chdlp=b&chd=t:14,86&cht=p&chdl=Non%20propos%C3%A9|Propos%C3%A9&chdlp=&chdls=000000,12" alt="chart p 2">
+            </div>
+        </div>        
+    </div>
+    <div>
+        <h3>Proposition de stage ayant le plus d'élève interessé</h3>
+        <img src="https://chart.apis.google.com/chart?chs=700x400&chd=t:4,8,3,7,1&cht=bhg&chco=ff9900|d6b79a&chds=a&chxt=y&chxl=0:|Stage%205|Stage%204|Stage%203|Stage%202|Stage%201&chbh=a" alt="chart b">
+    </div>
+</div>
