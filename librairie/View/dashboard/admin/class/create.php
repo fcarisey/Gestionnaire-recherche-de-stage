3ac58a3e5ef4a4e21f1f35db2cebbb7c0ee29e72
Frédéric Carisey <63855888+Venntus17@users.noreply.github.com>
2022-03-30 15:20:17 +0200

    up

--- librairie/View/dashboard/admin/class/create.php
+++ librairie/View/dashboard/admin/class/create.php
@@ -1,1 +1,101 @@
-<h2>dashboard/class/create</h2>
+<?php
+
+$classes = \Controller\ClasseController::SELECT(['idclasse', 'designation']);
+
+?>
+
+<style>
+    #da_class_create{
+        display: flex;
+    }
+
+    #da_class_create form{
+        margin: auto;
+        width: fit-content;
+    }
+
+    #da_class_create label{
+        display: flex;
+        flex-direction: column;
+    }
+
+    #da_class_create label > *{
+        min-width: 250px;
+        width: fit-content;
+    }
+
+    #create_input label > select{
+        min-width: 270px;
+    }
+
+    #create_input form > div{
+        display: flex;
+        justify-content: space-between;
+        width: 600px;
+    }
+
+    #create_input{
+        width: 70%;
+    }
+
+    #create_input form > div{
+        margin: 50px 0;
+    }
+
+    #create_file{
+        width: 25%;
+    }
+
+    #create_file form{
+        display: flex;
+        flex-direction: column;
+    }
+
+    #create_file form button{
+        margin-top: 20px;
+        width: fit-content;
+    }
+
+
+</style>
+
+<div id="da_class_create">
+    <div id="create_input">
+        <form name="create_input" onclick="da_class_create_input(events)">
+            <div>
+                <label>
+                    Prénom :
+                    <input autocomplete="none" required name="firstname" type="text" placeholder="John">
+                </label>
+                <label>
+                    Nom :
+                    <input autocomplete="none" required name="lastname" type="text" placeholder="Doe">
+                </label>
+            </div>
+            <div>
+                <label>
+                    Courriel :
+                    <input required name="firstname" type="email" placeholder="john.doe@exemple.com">
+                </label>
+                <label>
+                    Classe :
+                    <select required name="class">
+                        <option selected>Selectionner une classe</option>
+                        <optgroup>
+                            <?php foreach ($classes as $classe): ?>
+                                <option value="<?= $classe->getIdclasse() ?>"><?= $classe->getDesignation() ?></option>
+                            <?php endforeach ?>
+                        </optgroup>
+                    </select>
+                </label>
+            </div>
+            <button class='btn'>Créer</button>
+        </form>
+    </div>
+    <div id="create_file">
+        <form name="create_file" onclick="da_class_create_file(event)">
+            <input type="file" name="file" accept=".csv">
+            <button class="btn">Créer</button>
+        </form>
+    </div>
+</div>
--- librairie/View/dashboard/admin/class/home.php
+++ librairie/View/dashboard/admin/class/home.php
@@ -1,1 +1,85 @@
-<h2>dashboard/class</h2>
+<?php
+
+$classes = \Controller\ClasseController::SELECT(\Database::SELECT_ALL);
+
+?>
+
+<style>
+    #da_class_home table{
+        width: 70%;
+        margin: auto;
+    }
+
+    #da_class_home thead th{
+        border-bottom: 2px solid gray;
+    }
+
+    #da_class_home tbody td{
+        text-align: center;
+    }
+
+    #da_class_home tfoot th{
+        border-top: 2px solid gray;
+    }
+</style>
+
+<div id="da_class_home">
+    <table>
+        <thead>
+            <tr>
+                <th>Designation</th>
+                <th>Stage début</th>
+                <th>Stage fin</th>
+                <th>Professeur</th>
+                <th>Nombre élève</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($classes as $classe): ?>
+                <tr>
+                    <td><a href=""><?= $classe->getDesignation() ?></a></td>
+                    <td><?= $classe->getInternshipdatestart() ?></td>
+                    <td><?= $classe->getInternshipdateend() ?></td>
+
+                    <?php
+
+                        $teacherId = \Controller\AffiliateController::SELECT(['idteacher'], [
+                            'idclasse' => $classe->getIdclasse()
+                        ]);
+
+                        $teacher = null;
+                        if ($teacherId) {
+                            $teacherId = $teacherId[0]->getIdteacher();
+
+                            $teacher = \Controller\TeacherController::SELECT(['username'], [
+                                'idteacher' => $teacherId
+                            ])[0];
+                        }
+
+                    ?>
+
+                    <td><?= ($teacher) ? $teacher->getUsername() : null ?></td>
+
+                    <?php
+
+                        $nbEleve = \Database::$db_array['grds']->specialRequest("SELECT COUNT(*) as i FROM student WHERE idclasse = :idclasse", [
+                            ':idclasse' => $classe->getIdclasse()
+                        ])[0]['i'];
+
+                    ?>
+
+                    <td><?= $nbEleve ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Designation</th>
+                <th>Stage début</th>
+                <th>Stage fin</th>
+                <th>Professeur</th>
+                <th>Nombre élève</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
